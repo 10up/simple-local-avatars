bcede6fef573d8821067964609d6b83dcce7c1d1
Steve Grunwell <stevegrunwell@gmail.com>
2016-02-02 15:10:15 -0500

    Rewrite simple_local_avatars_uninstall() to adhere to the WordPress Coding Standards

--- simple-local-avatars.php
+++ simple-local-avatars.php
@@ -15,21 +15,20 @@ require_once dirname( __FILE__ ) . '/includes/template-tags.php';
 $simple_local_avatars = new Simple_Local_Avatars;
 
 /**
- * on uninstallation, remove the custom field from the users and delete the local avatars
+ * Upon installation, remove settings and local avatars from WordPress.
  */
-
-register_uninstall_hook( __FILE__, 'simple_local_avatars_uninstall' );
-
 function simple_local_avatars_uninstall() {
-	$simple_local_avatars = new Simple_Local_Avatars;
-	$users = get_users(array(
-		'meta_key'	=> 'simple_local_avatar',
-		'fields'	=> 'ids',
-	));
+	$instance = new Simple_Local_Avatars;
+	$users    = get_users( array(
+		'meta_key' => 'simple_local_avatar',
+		'fields'	 => 'ids',
+	) );
 
-	foreach ( $users as $user_id ):
-		$simple_local_avatars->avatar_delete( $user_id );
-	endforeach;
-	
-	delete_option('simple_local_avatars');
+	foreach ( $users as $user_id ) {
+		$instance->avatar_delete( $user_id );
+	}
+
+	// Remove settings from the database.
+	delete_option( 'simple_local_avatars' );
 }
+register_uninstall_hook( __FILE__, 'simple_local_avatars_uninstall' );
