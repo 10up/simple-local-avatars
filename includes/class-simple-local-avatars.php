09a933fab377fda489f9ca1f4fd557c6f39688c2
Vlad Olaru <vlad@pixelgrade.com>
2022-05-13 17:00:46 +0300

    Update includes/class-simple-local-avatars.php
    Co-authored-by: Faisal Alvi <faisalalvi33@gmail.com>

--- includes/class-simple-local-avatars.php
+++ includes/class-simple-local-avatars.php
@@ -358,7 +358,7 @@ class Simple_Local_Avatars {
 							$dest_file_url = '';
 							if ( false !== strpos( $dest_file, $upload_path['basedir'] ) ) {
 								$dest_file_url = str_replace( $upload_path['basedir'], $upload_path['baseurl'], $dest_file );
-							} else if ( is_multisite() && false !== strpos( $dest_file, ABSPATH . 'wp-content/uploads' ) ) {
+							} else if ( is_multisite() && strpos( $dest_file, ABSPATH . 'wp-content/uploads' ) ) {
 								$dest_file_url = str_replace( ABSPATH . 'wp-content/uploads', network_site_url( '/wp-content/uploads' ), $dest_file );
 							}
 
