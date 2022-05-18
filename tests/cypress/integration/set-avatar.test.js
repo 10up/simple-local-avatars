2a4f8f9a63af45bcf69eabd434b59da7e84de991
Faisal Alvi <faisal.alvi@get10up.com>
2022-05-18 12:54:49 +0530

    upload avatar in media and set from edit profile page

--- tests/cypress/integration/set-avatar.test.js
+++ tests/cypress/integration/set-avatar.test.js
@@ -1,8 +1,36 @@
-describe( 'Check if "Select avatar and Crop" popup appears', () => {
-  it( 'Can admin open "Select avatar and Crop" popup?', () => {
-    cy.login();
-    cy.visit( '/wp-admin/profile.php' );
-    cy.get( '#simple-local-avatar-media' ).click();
-    cy.get( '#media-frame-title h1' ).should( 'contain.text', 'Select avatar and Crop' );
-  } );
-} );
+import 'cypress-file-upload';
+
+describe('Check if "Select avatar and Crop" popup appears', () => {
+    before(() => {
+        cy.login();
+    });
+
+    it('Can admin open "Select avatar and Crop" popup?', () => {
+
+        // Check and upload at least one media file.
+        cy.visit('/wp-admin/upload.php?mode=list');
+        cy.get('body').then($body => {
+            if (0 !== $body.find('.no-items').length) {
+                cy.visit('/wp-admin/media-new.php?browser-uploader');
+                cy.get('#async-upload').attachFile('../../../.wordpress-org/icon-256x256.png');
+                cy.get('#html-upload').click();
+            }
+        });
+
+        // Visit profile page and set uploaded file as an avatar.
+        cy.visit( '/wp-admin/profile.php' );
+        cy.get( '#simple-local-avatar-media' ).click();
+
+        cy.get( '#menu-item-browse' ).click();
+        cy.get( 'li.attachment.save-ready:first-child' ).click();
+        cy.get( '.media-button-select' ).click();
+
+        cy.get('body').then($body => {
+            if (0 !== $body.find('.media-button-insert').length) {
+                cy.get('.media-button-insert').click();
+            }
+        });
+
+        cy.get( '#simple-local-avatar-remove' ).should( 'be.visible' );
+    });
+});
