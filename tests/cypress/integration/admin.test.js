b5d6a01e902e6e35f4ad0a8adcd1512bbf336a20
JK <jayedulk@gmail.com>
2022-12-07 15:26:00 +0600

    Cypress integration migrated to 11+

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,8 +1,14 @@
 import {deactivatePlugin} from "@10up/cypress-wp-utils/lib/commands/deactivate-plugin";
 
 describe('Admin can login and make sure plugin is activated', () => {
+	before(() => {
+        cy.login();
+    });
+
     it('Can activate plugin if it is deactivated', () => {
-        cy.activatePlugin('simple-local-avatars');
-        cy.deactivatePlugin('simple-local-avatars');
+		cy.visitAdminPage("plugins.php");
+        cy.get("#deactivate-simple-local-avatars, tr[data-slug=simple-local-avatars] .deactivate a").click();
+		cy.get("#activate-simple-local-avatars, tr[data-slug=simple-local-avatars] .activate a").click();
+		cy.get("#deactivate-simple-local-avatars, tr[data-slug=simple-local-avatars] .deactivate a").should("be.visible");
     });
 });
