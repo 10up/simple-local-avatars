d6d1fc245038e4c9c1c62cd763ab7927fc68f47e
JK <jayedulk@gmail.com>
2022-12-12 13:31:03 +0600

    Plugin activation deactivation fix

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,14 +1,10 @@
-import {deactivatePlugin} from "@10up/cypress-wp-utils/lib/commands/deactivate-plugin";
-
 describe('Admin can login and make sure plugin is activated', () => {
     before(() => {
         cy.login();
     });
 
     it('Can activate plugin if it is deactivated', () => {
-        cy.visitAdminPage("plugins.php");
-        cy.get("#deactivate-simple-local-avatars, tr[data-slug=simple-local-avatars] .deactivate a").click();
-        cy.get("#activate-simple-local-avatars, tr[data-slug=simple-local-avatars] .activate a").click();
-        cy.get("#deactivate-simple-local-avatars, tr[data-slug=simple-local-avatars] .deactivate a").should("be.visible");
+        cy.deactivatePlugin('simple-local-avatars');
+        cy.activatePlugin('simple-local-avatars');
     });
 });
