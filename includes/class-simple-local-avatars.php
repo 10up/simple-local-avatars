4c5aa66a2b9dbf4eba8c4931d640c893834825de
Peter Wilson <git-commit-by@peterwilson.cc>
2026-02-03 12:22:11 +1100

    $SERVER needs unslashing.

--- includes/class-simple-local-avatars.php
+++ includes/class-simple-local-avatars.php
@@ -75,7 +75,7 @@ class Simple_Local_Avatars {
 				( // And either an ajax request not in the network admin.
 					defined( 'DOING_AJAX' ) && DOING_AJAX
 					&& isset( $_SERVER['HTTP_REFERER'] )
-					&& ! preg_match( '#^' . preg_quote( network_admin_url(), '#' ) . '#i', $_SERVER['HTTP_REFERER'] )
+					&& ! preg_match( '#^' . preg_quote( network_admin_url(), '#' ) . '#i', wp_unslash( $_SERVER['HTTP_REFERER'] ) )
 				)
 				||
 				( // Or normal request not in the network admin.
