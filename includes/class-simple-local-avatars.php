3a01d7e3b8d41f6310ccc75e678aa204278e0261
Peter Wilson <git-commit-by@peterwilson.cc>
2026-02-03 12:30:41 +1100

    Check nonce used in WP Core before unhooking actions.

--- includes/class-simple-local-avatars.php
+++ includes/class-simple-local-avatars.php
@@ -149,14 +149,17 @@ class Simple_Local_Avatars {
 		 * Fix: An error occurred cropping the image
 		 *
 		 * @see https://github.com/10up/simple-local-avatars/issues/141
+		 * @see wp_ajax_crop_image() in wp-admin/includes/ajax-actions.php
 		 *
 		 * During a WordPress Core crop-image ajax request, bypass the theme_setup hook.
 		 */
 		if (
 			isset( $_POST['action'] )
 			&& 'crop-image' === $_POST['action']
+			&& isset( $_POST['id'] )
 			&& is_admin()
 			&& wp_doing_ajax()
+			&& check_ajax_referer( 'image_editor-' . absint( $_POST['id'] ), 'nonce', false )
 		) {
 			add_action( 'plugins_loaded', function () {
 				remove_all_actions( 'setup_theme' );
